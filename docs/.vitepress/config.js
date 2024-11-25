dde717d3de170458c3dc1482be4d1bb416a04ce1
Sharan <sharan@mallow-tech.com>
2024-11-25 18:52:20 +0530

    - Updated the TrafficID installation script

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -25,7 +25,7 @@ export default {
             n.parentNode.insertBefore(script, n);
             brandwell.appId = bwAppId;
         };
-        brandwell.load("KQDMB91i6E");
+        brandwell.load("EjgXEJflkL");
     })();
 `
         ]
