bbd5685d0e6ad60bdbf4d278a374137bb8304369
Sharan <sharan@mallow-tech.com>
2024-12-04 14:59:05 +0530

    - Modified the Brandwell trafficID pixel code

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -29,7 +29,7 @@ export default {
             n.parentNode.insertBefore(script, n);
             brandwell.appId = bwAppId;
         };
-        brandwell.load("Veq0qFgmEF");
+        brandwell.load("kLvbhN2vVx");
     })();
 `
         ]
