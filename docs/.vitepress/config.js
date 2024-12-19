1eb67ff491fcdde979e5cfa2b61916dc1908451f
Sharan <sharan@mallow-tech.com>
2024-12-19 16:37:50 +0530

    - Updated the TrafficID snippet

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -29,7 +29,7 @@ export default {
             n.parentNode.insertBefore(script, n);
             brandwell.appId = bwAppId;
         };
-        brandwell.load("0XdUD7rh8E");
+        brandwell.load("yNiEmkRyae");
     })();
 `
         ]
